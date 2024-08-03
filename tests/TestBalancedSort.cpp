b0022371758b8fe3f1b4f79a4105ac6c08895df9
IgorPBorja <igorpradoborja@gmail.com>
2024-08-03 20:14:57 -0300

    tests: larger random testing for balanced sort

--- tests/TestBalancedSort.cpp
+++ tests/TestBalancedSort.cpp
@@ -62,13 +62,49 @@ TEST(test_balanced_sort, parametrized_small_random_test_sort) {
 }
 
 TEST(test_balanced_sort, parametrized_large_random_test_sort) {
-    // FIXME: TAKING TOO LONG
-    // GTEST_SKIP() << "Skipping this test since it takes 100% CPU usage and still does not finish" << endl;
+    for (uint i = 0; i < 100; i++) {
+        const uint num_files = 2 * RandomDataFixture::randint(2, 30);
+        const uint mem_size = RandomDataFixture::randint( num_files + 1, std::min(num_files * 2 + 1, 70u));
+        const uint size = RandomDataFixture::randint(1e4, 5e4);
+        const vector<int> data = RandomDataFixture::random_vector(size, -1e5, +1e5);
+        const vector<int> balanced_sorted_data = balanced_sort(data, num_files, mem_size, false);
+        vector<int> expected_sorted_data = data;
+        sort(expected_sorted_data.begin(), expected_sorted_data.end());
+
+        SCOPED_TRACE("FAILED TESTCASE " + std::to_string(i) + ":\nInput: " + Debugging::to_string(data) + "\nOutput: "
+            + Debugging::to_string(balanced_sorted_data)
+            + "\nExpected: " + Debugging::to_string(expected_sorted_data));
+        ASSERT_EQ(
+            balanced_sorted_data, expected_sorted_data
+        );
+    }
+}
+
+TEST(test_balanced_sort, parametrized_huge_random_test_sort) {
     for (uint i = 0; i < 10; i++) {
         const uint num_files = 2 * RandomDataFixture::randint(2, 30);
         const uint mem_size = RandomDataFixture::randint( num_files + 1, std::min(num_files * 2 + 1, 70u));
-        const uint size = RandomDataFixture::randint(100, 200);
-        const vector<int> data = RandomDataFixture::random_vector(size, -1e5, +1e5);
+        const uint size = RandomDataFixture::randint(1e5, 3e5);
+        const vector<int> data = RandomDataFixture::random_vector(size, -1e9, +1e9);
+        const vector<int> balanced_sorted_data = balanced_sort(data, num_files, mem_size, false);
+        vector<int> expected_sorted_data = data;
+        sort(expected_sorted_data.begin(), expected_sorted_data.end());
+
+        SCOPED_TRACE("FAILED TESTCASE " + std::to_string(i) + ":\nInput: " + Debugging::to_string(data) + "\nOutput: "
+            + Debugging::to_string(balanced_sorted_data)
+            + "\nExpected: " + Debugging::to_string(expected_sorted_data));
+        ASSERT_EQ(
+            balanced_sorted_data, expected_sorted_data
+        );
+    }
+}
+
+TEST(test_balanced_sort, parametrized_mostly_equal_huge_random_test_sort) {
+    for (uint i = 0; i < 10; i++) {
+        const uint num_files = 2 * RandomDataFixture::randint(2, 30);
+        const uint mem_size = RandomDataFixture::randint( num_files + 1, std::min(num_files * 2 + 1, 70u));
+        const uint size = RandomDataFixture::randint(1e5, 3e5);
+        const vector<int> data = RandomDataFixture::random_vector(size, -100, +100);
         const vector<int> balanced_sorted_data = balanced_sort(data, num_files, mem_size, false);
         vector<int> expected_sorted_data = data;
         sort(expected_sorted_data.begin(), expected_sorted_data.end());
