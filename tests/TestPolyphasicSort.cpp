aabf0e1d4b4ae4009d19c58daa0594ab98b7f328
IgorPBorja <igorpradoborja@gmail.com>
2024-08-03 17:53:02 -0300

    tests: add random test for huge vectors

--- tests/TestPolyphasicSort.cpp
+++ tests/TestPolyphasicSort.cpp
@@ -59,8 +59,28 @@ TEST(test_polyphasic_sort, parametrized_large_random_test_sort) {
     }
 }
 
-TEST(test_balanced_sort, parametrized_small_random_test_sort) {
+TEST(test_polyphasic_sort, parametrized_huge_random_test_sort) {
     for (uint i = 0; i < 10; i++) {
+        const uint num_files = 2 * RandomDataFixture::randint(2, 10);
+        const uint mem_size = RandomDataFixture::randint(num_files + 1, 2 * num_files + 1);
+        const uint size = RandomDataFixture::randint(1e5, 3e5);
+        const vector<int> data = RandomDataFixture::random_vector(size, -1e9, +1e9);
+        const vector<int> balanced_sorted_data = polyphasic_sort(data, num_files, mem_size, false);
+        vector<int> expected_sorted_data = data;
+        sort(expected_sorted_data.begin(), expected_sorted_data.end());
+
+        SCOPED_TRACE("FAILED TESTCASE " + std::to_string(i) + ":\nInput: " + Debugging::to_string(data) + "\nOutput: "
+            + Debugging::to_string(balanced_sorted_data)
+            + "\nExpected: " + Debugging::to_string(expected_sorted_data));
+        ASSERT_EQ(
+            balanced_sorted_data, expected_sorted_data
+        );
+    }
+
+}
+
+TEST(test_polyphasic_sort, parametrized_small_random_test_sort) {
+    for (uint i = 0; i < 1000; i++) {
         const uint num_files = 2 * RandomDataFixture::randint(2, 10);
         const uint mem_size = RandomDataFixture::randint(num_files + 1, 2 * num_files + 1);
         const uint size = RandomDataFixture::randint(1, 10);
